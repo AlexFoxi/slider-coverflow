41fdf9d7b5c1b69532edeef9a2def06f3dc709ad
AlexK <blojs.lj@gmail.com>
2025-03-10 14:15:01 +0200

    slider view improvements, new arrows

--- js/main.js
+++ js/main.js
@@ -1,96 +1,105 @@
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
 const customSlider = (name, opts) => {
-	const slider = document.querySelector(`.${name}`)
-	const sliderWrapper = slider.querySelector('.slider-wrapper')
+  const slider = document.querySelector(`.${name}`)
+  const sliderWrapper = slider.querySelector('.slider-wrapper')
 
-	const duration = opts.duration
-	const slideWidth = opts.width
-	const space = opts.space
+  const duration = opts.duration
+  const slideWidth = opts.width
+  const slideHeight = opts.height
+  const space = opts.space
 
-	let isMoving = false
+  let isMoving = false
 
-	const actionHelper = (slides, slide, i) => {
-		if (i > 1) {
-			slide.style.left = `calc(50% + ${(i - 2) * (slideWidth + space)}rem)`
-			slide.style.width = `${slideWidth}rem`
-			slide.style.height = `${slideWidth}rem`
-		} else {
-			slide.style.left = `0rem`
-			slide.style.width = '100%'
-			slide.style.height = '100%'
-		}
+  const actionHelper = (slides, slide, i) => {
+    if (i > 1) {
+      slide.style.left = `calc(50% + ${(i - 2) * (slideWidth + space)}rem)`
+      slide.style.width = `${slideWidth}rem`
+      slide.style.height = `${slideHeight}rem`
+    } else {
+      slide.style.left = `0rem`
+      slide.style.width = '100%'
+      slide.style.height = '100%'
+    }
 
-		i == slides.length - 1
-			? (slide.style.opacity = '0')
-			: (slide.style.opacity = '1')
-	}
+    if (i == 1) {
+      setTimeout(() => {
+        slide.classList.add('observed')
+      }, duration)
+    }
 
-	const setSlidesBase = () => {
-		const slides = Array.from(sliderWrapper.children)
+    i == slides.length - 1
+      ? (slide.style.opacity = '0')
+      : (slide.style.opacity = '1')
+  }
 
-		slides.map((slide, i) => {
-			actionHelper(slides, slide, i)
+  const setSlidesBase = () => {
+    const slides = Array.from(sliderWrapper.children)
 
-			setTimeout(() => {
-				slide.style.transition = `all ${duration}ms ease-in-out`
-			}, 0)
-		})
-	}
+    slides.map((slide, i) => {
+      actionHelper(slides, slide, i)
 
-	const slidesActions = () => {
-		const slides = Array.from(sliderWrapper.children)
+      setTimeout(() => {
+        slide.style.transition = `all ${duration}ms ease-in-out`
+      }, 0)
+    })
+  }
 
-		slides.map((slide, i) => {
-			actionHelper(slides, slide, i)
-		})
-	}
+  const slidesActions = () => {
+    const slides = Array.from(sliderWrapper.children)
 
-	const moveHandler = (direction) => {
-		if (isMoving) return
-		isMoving = true
+    slides.forEach(slide => {
+      slide.classList.contains('observed') && slide.classList.remove('observed')
+    })
 
-		const slides = Array.from(sliderWrapper.children)
+    slides.map((slide, i) => {
+      actionHelper(slides, slide, i)
+    })
+  }
 
-		if (direction === 'left') {
-			sliderWrapper.prepend(slides[slides.length - 1])
-		} else if (direction === 'right') {
-			sliderWrapper.appendChild(slides[0])
-		}
-		slidesActions()
-	}
+  const moveHandler = direction => {
+    if (isMoving) return
+    isMoving = true
 
-	sliderWrapper.addEventListener('transitionend', () => {
-		isMoving = false
-	})
+    const slides = Array.from(sliderWrapper.children)
 
-	const mouseEvents = (slider) => {
-		const arrowLeft = slider.querySelector(`.${opts.navigation.left}`)
-		const arrowRight = slider.querySelector(`.${opts.navigation.right}`)
+    if (direction === 'left') {
+      sliderWrapper.prepend(slides[slides.length - 1])
+    } else if (direction === 'right') {
+      sliderWrapper.appendChild(slides[0])
+    }
+    slidesActions()
+  }
 
-		arrowLeft.addEventListener('click', () => moveHandler('left'))
-		arrowRight.addEventListener('click', () => moveHandler('right'))
-	}
+  sliderWrapper.addEventListener('transitionend', e => {
+    if (e.propertyName === 'width') {
+      isMoving = false
+    }
+  })
 
-	const initSlider = (slider) => {
-		setSlidesBase()
-		mouseEvents(slider)
-	}
+  const mouseEvents = slider => {
+    const arrowLeft = slider.querySelector(`.${opts.navigation.left}`)
+    const arrowRight = slider.querySelector(`.${opts.navigation.right}`)
 
-	initSlider(slider)
+    arrowLeft.addEventListener('click', () => moveHandler('left'))
+    arrowRight.addEventListener('click', () => moveHandler('right'))
+  }
+
+  const initSlider = slider => {
+    setSlidesBase()
+    mouseEvents(slider)
+  }
+
+  initSlider(slider)
 }
 
 window.addEventListener('load', () => {
-	customSlider('custom_slider', {
-		duration: 500,
-		width: 25,
-		space: 2,
-		navigation: {
-			left: 'arrow_left',
-			right: 'arrow_right',
-		},
-	})
+  customSlider('custom_slider', {
+    duration: 500,
+    width: 25,
+    height: 15,
+    space: 2,
+    navigation: {
+      left: 'arrow_left',
+      right: 'arrow_right'
+    }
+  })
 })
-
-/******/ })()
-;
